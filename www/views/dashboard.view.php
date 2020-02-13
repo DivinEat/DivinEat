230f3f94bdf54f4023c02a5d51abeb03f4fd7a17
tdargent1 <tdargent1@gmail.com>
2020-02-13 15:04:29 +0100

    Ajout des cadres pour la view Dashboard + SCSS

---
+++ www/views/dashboard.view.php
@@ -0,0 +1,124 @@
+<select class="form-select" name="stats-date">
+    <option value="today">Aujourd'hui</option>
+    <option value="month">Mois en cours</option>
+    <option value="3LastMonth">3 derniers mois</option>
+    <option value="year">Cette année</option>
+    <option value="allTime">Toujours</option>
+</select>
+
+<div class="row">
+    <div class="col-sm-4">
+        <div class="col-inner">
+            <article class="bg-purple">
+                <figure>
+                    <figcaption class="stats">
+                        <h1>19</h1>
+                        <h2>En cours</h2>
+                    </figcaption>
+                </figure>
+            </article>
+        </div>
+    </div>
+    <div class="col-sm-4">
+        <div class="col-inner">
+            <article class="bg-white">
+                <figure>
+                    <figcaption class="stats">
+                        <h1>11</h1>
+                        <h2>Sur place</h2>
+                    </figcaption>
+                </figure>
+            </article>
+        </div>
+    </div>
+    <div class="col-sm-4">
+        <div class="col-inner">
+            <article class="bg-light_purple">
+                <figure>
+                    <figcaption class="stats">
+                        <h1>58</h1>
+                        <h2>A emporter</h2>
+                    </figcaption>
+                </figure>
+            </article>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-sm-4">
+        <div class="col-inner">
+            <article class="bg-light_purple">
+                <figure>
+                    <figcaption class="stats">
+                        <h1>8</h1>
+                        <h2>Sur place</h2>
+                    </figcaption>
+                </figure>
+            </article>
+        </div>
+    </div>
+    <div class="col-sm-4">
+        <div class="col-inner">
+            <article class="bg-purple">
+                <figure>
+                    <figcaption class="stats">
+                        <h1>58</h1>
+                        <h2>Terminées</h2>
+                    </figcaption>
+                </figure>
+            </article>
+        </div>
+    </div>
+    <div class="col-sm-4">
+        <div class="col-inner">
+            <article class="bg-white">
+                <figure>
+                    <figcaption class="stats">
+                        <h1>77</h1>
+                        <h2>Total</h2>
+                    </figcaption>
+                </figure>
+            </article>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-sm-4">
+        <div class="col-inner">
+            <article class="bg-white">
+                <figure>
+                    <figcaption class="stats">
+                        <h1>654,33 €</h1>
+                        <h2>Chiffre d'affaires</h2>
+                    </figcaption>
+                </figure>
+            </article>
+        </div>
+    </div>
+    <div class="col-sm-4">
+        <div class="col-inner">
+            <article class="bg-light_purple">
+                <figure>
+                    <figcaption class="stats">
+                        <h1>307</h1>
+                        <h2>Visiteurs</h2>
+                    </figcaption>
+                </figure>
+            </article>
+        </div>
+    </div>
+    <div class="col-sm-4">
+        <div class="col-inner">
+            <article class="bg-purple">
+                <figure>
+                    <figcaption class="stats">
+                        <h1>4</h1>
+                        <h2>Nouveaux clients</h2>
+                    </figcaption>
+                </figure>
+            </article>
+        </div>
+    </div>
+</div>
