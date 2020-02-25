028f3db154f33e96dca335fec52daba591f25948
Taopol BERQUER <taopol.berquer@rampar.com>
2020-02-25 11:43:45 +0100

    Ajout d'une premiere version des entrées et des plats

--- www/views/menus.view.php
+++ www/views/menus.view.php
@@ -80,79 +80,67 @@
         </div>
     </div>
     <div class="col-sm-12 col-md-6 frame-menus">
-        <div class="row"><span>Entrées</span></div>
-        <div class="ligne"></div>
+        <div>
+            <div class="row"><span>Entrées</span></div>
+            <div class="ligne"></div>
 
-        <div class="row">
-            <div class="col-sm-12 col-md-6">
-                <div class="col-inner">
-                    <div class="menu">
-                        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum justo vitae metus</div>
-                        <div>13€</div>
-                    </div>
-                    <div class="menu">
-                        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum justo vitae metus</div>
-                        <div>18€</div>
-                    </div>
-                    <div class="menu">
-                        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum justo vitae metus</div>
-                        <div>15€</div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-12 col-md-6">
-                <div class="col-inner">
-                    <div class="menu">
-                        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum justo vitae metus</div>
-                        <div>13€</div>
-                    </div>
-                    <div class="menu">
-                        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum justo vitae metus</div>
-                        <div>18€</div>
-                    </div>
-                    <div class="menu">
-                        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum justo vitae metus</div>
-                        <div>15€</div>
+            <div class="row">
+                <div class="col-sm-8">
+                    <div class="col-inner">
+                        <div class="menu">
+                            <div>Tartare de Saumon à l'Avocat</div>
+                            <div>11€</div>
+                        </div>
+                        <div class="menu">
+                            <div>Velouté de Courges et éclats de Châtaignes</div>
+                            <div>9€</div>
+                        </div>
+                        <div class="menu">
+                            <div>Ravioles du Dauphiné Label Rouge gratinées au Parmesan</div>
+                            <div>11€</div>
+                        </div>
+                        <div class="menu">
+                            <div>Ballotine de Foie Gras de Castelnau, toasts mystères, confits d'oignons et confiture Corse et noix</div>
+                            <div>16€</div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
     <div class="col-sm-12 frame-menus">
-        <div class="row"><span>Plats</span></div>
-        <div class="ligne"></div>
+        <div>
+            <div class="row"><span>Plats</span></div>
+            <div class="ligne"></div>
 
-        <div class="row">
-            <div class="col-sm-12 col-md-6">
-                <div class="col-inner">
-                    <div class="menu">
-                        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum justo vitae metus</div>
-                        <div>13€</div>
-                    </div>
-                    <div class="menu">
-                        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum justo vitae metus</div>
-                        <div>18€</div>
-                    </div>
-                    <div class="menu">
-                        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum justo vitae metus</div>
-                        <div>15€</div>
+            <div class="row">
+                <div class="col-sm-8">
+                    <div class="col-inner">
+                        <div class="menu">
+                            <div>Dos de Cabillaud Frais, sauce vierge et sa Ratatouille Provençale</div>
+                            <div>20€</div>
+                        </div>
+                        <div class="menu">
+                            <div>Onglet de Boeuf (200g.) sauce aux Cèpes et Aligot maison</div>
+                            <div>21€</div>
+                        </div>
+                        <div class="menu">
+                            <div>Noix de Saint Jacques fraîches rôties à la plancha, Fondue de Poireaux et son Coulis de Betterave</div>
+                            <div>24€</div>
+                        </div>
+                        <div class="menu">
+                            <div>Lasagne Végetarienne Ricotta et épinards, Salade de jeunes pousses</div>
+                            <div>17€</div>
+                        </div>
+                        <div class="menu">
+                            <div>Burger Rupins : Steak Black Angus (150g.), St Nectaire Fermier, confits d'oignons, pousses d'épinards et sauce secrète</div>
+                            <div>19€</div>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-sm-12 col-md-6">
-                <div class="col-inner">
-                    <div class="menu">
-                        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum justo vitae metus</div>
-                        <div>13€</div>
-                    </div>
-                    <div class="menu">
-                        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum justo vitae metus</div>
-                        <div>18€</div>
-                    </div>
-                    <div class="menu">
-                        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum justo vitae metus</div>
-                        <div>15€</div>
-                    </div>
+                <div class="col-sm-4 row text-center">
+                    <div><img class="width-50" src="/public/img/menus/plats.jpg"></div>
+                    <div><img class="width-50" src="/public/img/menus/plats.jpg"></div>
                 </div>
             </div>
         </div>
