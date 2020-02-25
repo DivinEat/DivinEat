a05442fc71d3d06d9e288d1411cb17332fdcca2f
tdargent1 <tdargent1@gmail.com>
2020-02-25 11:41:52 +0100

    Boisson + Dessert

--- www/views/menus.view.php
+++ www/views/menus.view.php
@@ -41,39 +41,40 @@
         </div>
     </div>
     <div class="col-sm-12 col-md-6 frame-menus">
-        <div class="row"><span>Boissons</span></div>
-        <div class="ligne"></div>
-
-        <div class="row">
-            <div class="col-sm-12 col-md-6">
-                <div class="col-inner">
-                    <div class="menu">
-                        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum justo vitae metus</div>
-                        <div>13€</div>
-                    </div>
-                    <div class="menu">
-                        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum justo vitae metus</div>
-                        <div>18€</div>
-                    </div>
-                    <div class="menu">
-                        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum justo vitae metus</div>
-                        <div>15€</div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-12 col-md-6">
-                <div class="col-inner">
-                    <div class="menu">
-                        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum justo vitae metus</div>
-                        <div>13€</div>
-                    </div>
-                    <div class="menu">
-                        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum justo vitae metus</div>
-                        <div>18€</div>
-                    </div>
-                    <div class="menu">
-                        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum justo vitae metus</div>
-                        <div>15€</div>
+        <div>
+            <div class="row"><span>Boissons</span></div>
+            <div class="ligne"></div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="col-inner">
+                        <div class="menu">
+                            <div>Bouteille de champagne</div>
+                            <div>65€</div>
+                        </div>
+                        <div class="menu">
+                            <div>Verre de champagne</div>
+                            <div>10€</div>
+                        </div>
+                        <div class="menu">
+                            <div>Café</div>
+                            <div>2.3€</div>
+                        </div>
+                        <div class="menu">
+                            <div>Bouteille d'eau</div>
+                            <div>6€</div>
+                        </div>
+                        <div class="menu">
+                            <div>Demi-bouteille d'eau</div>
+                            <div>4€</div>
+                        </div>
+                        <div class="menu">
+                            <div>Bouteille de vin</div>
+                            <div>24€</div>
+                        </div>
+                        <div class="menu">
+                            <div>Verre de vin</div>
+                            <div>4.5€</div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -158,39 +159,39 @@
         </div>
     </div>
     <div class="col-sm-12 frame-menus">
-        <div class="row"><span>Desserts</span></div>
-        <div class="ligne"></div>
+        <div>
+            <div class="row"><span>Desserts</span></div>
+            <div class="ligne"></div>
 
-        <div class="row">
-            <div class="col-sm-12 col-md-6">
-                <div class="col-inner">
-                    <div class="menu">
-                        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum justo vitae metus</div>
-                        <div>13€</div>
-                    </div>
-                    <div class="menu">
-                        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum justo vitae metus</div>
-                        <div>18€</div>
-                    </div>
-                    <div class="menu">
-                        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum justo vitae metus</div>
-                        <div>15€</div>
+            <div class="row">
+                <div class="col-sm-12 col-md-6">
+                    <div class="col-inner">
+                        <div class="menu">
+                            <div>Crème brulée maison</div>
+                            <div>8€</div>
+                        </div>
+                        <div class="menu">
+                            <div>Tiramisu du chef maison</div>
+                            <div>8€</div>
+                        </div>
+                        <div class="menu">
+                            <div>Moelleux au chocolat et Schocko-bon aison</div>
+                            <div>8€</div>
+                        </div>
+                        <div><img src="public/img/menus/buche.jpg" style="width:50px"></div>
+                        <div><img src="public/img/menus/buche.jpg" style="width:50px"></div>
                     </div>
                 </div>
-            </div>
-            <div class="col-sm-12 col-md-6">
-                <div class="col-inner">
-                    <div class="menu">
-                        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum justo vitae metus</div>
-                        <div>13€</div>
-                    </div>
-                    <div class="menu">
-                        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum justo vitae metus</div>
-                        <div>18€</div>
-                    </div>
-                    <div class="menu">
-                        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum justo vitae metus</div>
-                        <div>15€</div>
+                <div class="col-sm-12 col-md-6">
+                    <div class="col-inner">
+                        <div class="menu">
+                            <div>Café (très) gourmand maison</div>
+                            <div>9€</div>
+                        </div>
+                        <div class="menu">
+                            <div>Brioche égarée caramel beurre salé</div>
+                            <div>9€</div>
+                        </div>
                     </div>
                 </div>
             </div>
