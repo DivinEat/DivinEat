ca897d50e84aca79cd67ed8e73070bb7f26ecab0
Taopol BERQUER <taopol.berquer@rampar.com>
2020-02-27 12:27:51 +0100

    Ajout du constantLoader

--- src/Core/ConstantLoader.php
+++ src/Core/ConstantLoader.php
@@ -4,4 +4,26 @@ namespace Src\Core;
 
 class ConstantLoader
 {
+    public function __construct()
+    {
+        if ($this->isEnvFileExist())
+            $this->createConstantFromEnvFile();
+    }
+
+    protected function isEnvFileExist(): bool
+    {
+        if (!file_exists('../.env'))
+            throw new \Exception('Aucun fichier d\'environement détecté.');
+
+        return true;
+    }
+
+    protected function createConstantFromEnvFile(): void
+    {
+        array_map(function ($line) {
+            $explodedLine = explode('=', $line);
+            if (isset($explodedLine[0], $explodedLine[1]) && !defined($explodedLine[0], $explodedLine[1]))
+                define($explodedLine[0], $explodedLine[1]);
+        },explode('\n', file_get_contents('../.env')));
+    }
 }
