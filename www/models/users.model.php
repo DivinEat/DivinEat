f543b3e5e61979689ca26534cdad460e4c946a5d
Rémi Brat <remi.brat@outlook.fr>
2020-04-27 14:37:16 +0200

    [ADD] Hydrate function in users.model

--- www/models/users.model.php
+++ www/models/users.model.php
@@ -40,6 +40,16 @@ class users extends DB
         $this->status=$status;
     }
 
+    public function hydrate(array $donnees) {
+        foreach($donnees as $key=>$value){
+            $method = 'set'.ucfirst($key);
+
+            if(method_exists($this,$method)){
+                $this->$method($value);
+            }
+        }
+    }
+
 
 
     public static function getRegisterForm(){
