5584158a50ba172c3b63d1455944727054b8bc3e
tdargent1 <tdargent1@gmail.com>
2020-05-05 16:12:43 +0200

    Ajout des views pour installation du CMS (configuration bdd + compte admin)

--- www/controllers/TestController.class.php
+++ www/controllers/TestController.class.php
@@ -5,4 +5,12 @@ class TestController
     public function defaultAction() {
         $view = new View("home", "front");
     }
+
+    public function bddAction() {
+        $view = new View("install_bdd", "account");
+    }
+
+    public function adminAction() {
+        $view = new View("install_admin", "account");
+    }
 }
