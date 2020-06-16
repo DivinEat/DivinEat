829d8813d1d6a19a62c1289f086c9a9402c7a546
LudovicCollignon <lud.collignon@gmail.com>
2020-06-16 12:13:18 +0200

    init page builder

--- ressources/views/pageBuilder.php
+++ ressources/views/pageBuilder.php
@@ -13,30 +13,9 @@
                 <div class="row padding-0">
                     <div class="col-sm-12 padding-0">
                         <div class="col-inner padding-0">
-                            <div class="row padding-0">
-                                <div class="col-sm-12 padding-0">
-                                    <div class="col-inner padding-0">
-                                        <div class="pageBuilder-container-hidden">
-                                            <div class="add-container-button">+</div>
-                                            <!-- <div class="txt-editor">
-                                                <div class="txt-editor-header">
-                                                    <div
-                                                        class="txt-editor-btn txt-editor-btn-inactive txt-editor-btn-bold">
-                                                    </div>
-                                                    <div
-                                                        class="txt-editor-btn txt-editor-btn-active txt-editor-btn-italic">
-                                                    </div>
-                                                    <div
-                                                        class="txt-editor-btn txt-editor-btn-inactive txt-editor-btn-underline">
-                                                    </div>
-                                                </div>
-                                                <div class="txt-editor-separator"></div>
-                                                <textarea class="txt-editor-text-area" placeholder="Entrez votre texte ..."></textarea>
-                                            </div> -->
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            <div class="pageBuilder-container-hidden">
+                                <div class="pageBuilder-btn-add">+</div>
+                            </div>           
                         </div>
                     </div>
                 </div>
