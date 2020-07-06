0b45c7ab7090e63470152ba56e4a9905f5a8851f
tdargent1 <tdargent1@gmail.com>
2020-07-06 13:55:12 +0200

    Update form Menu

--- ressources/views/admin/menu/edit.php
+++ ressources/views/admin/menu/edit.php
@@ -3,19 +3,10 @@
         <div class="col-inner">
             <div class="box">
                 <div class="box-header with-border">
-                    <h3 class="box-title">Modifier un menu</h3>
+                    <h3 class="box-title">Modifier un <?= $name ?></h3>
                 </div>
                 <div class="box-body">
-                    <?php 
-                        $infos = [
-                            "entrees" => $entrees,
-                            "plats" => $plats,
-                            "desserts" => $desserts,
-                            "categorie" => $categorie,
-                            "object" => $object
-                        ];
-                        $this->addModal("menu_form", $configFormMenu, $infos);
-                    ?>
+                    <?php $this->formView("updateMenuForm", "menu", "updateMenuForm"); ?>
                 </div>
             </div>
         </div>
---
+++ ressources/views/forms/Menu/updateMenuForm.php
@@ -0,0 +1,21 @@
+<?php
+$elements = $form->getBuilder()->getElements();
+
+$displaySelect = "block";
+$displayInput = "none";
+?>
+
+<?php foreach ($elements as $element) : ?>
+    <?php if ($element->getName() != "createMenuForm_id") : ?>
+        <div class="form-group row">
+            <div class="col-sm-12">
+    <?php endif; ?>
+            <?php
+            $options = $element->getOptions();
+            include $element->getLayoutPath();
+            ?>
+    <?php if ($element->getName() !== "createMenuForm_id") : ?>
+            </div>
+        </div>
+    <?php endif; ?>
+<?php endforeach; ?>
