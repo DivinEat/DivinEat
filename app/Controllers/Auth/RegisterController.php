0959dfdee8fd1a818abedab56fc1b9f3f0a0c8d6
Taopol BERQUER <tberquer@rampar.com>
2020-07-09 16:03:13 +0200

    Ajout du controller Register.

--- app/Controllers/Auth/RegisterController.php
+++ app/Controllers/Auth/RegisterController.php
@@ -5,7 +5,11 @@ namespace App\Controllers\Auth;
 use App\Core\Controller\Controller;
 use App\Core\Http\Request;
 use App\Core\Http\Response;
+use App\Core\Routing\Router;
 use App\Forms\Auth\RegisterForm;
+use App\Managers\RoleManager;
+use App\Managers\UserManager;
+use App\Models\User;
 
 class RegisterController extends Controller
 {
@@ -18,6 +22,23 @@ class RegisterController extends Controller
 
     public function register(Request $request, Response $response)
     {
-        echo "register";
+        $request->setInputPrefix('registerForm_');
+        $userManager = new UserManager();
+
+        $user = $userManager->findBy(['email' => $request->get('email')]);
+
+        //TODO : Thibault modifie avec le form builder
+        if ([] !== $user || $request->get('pwd') !== $request->get('pwdConfirm'))
+            return Router::redirect('auth.register');
+
+        $userManager->save((new User())->hydrate([
+            'firstname' => $request->get('firstname'),
+            'lastname' => $request->get('lastname'),
+            'email' => $request->get('email'),
+            'pwd' => password_hash($request->get('pwd'), PASSWORD_DEFAULT),
+            'role' => current((new RoleManager())->findBy(['libelle' => 'Membre']))->getId(),
+        ]));
+
+        return Router::redirect('home');
     }
 }
