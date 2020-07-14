b4327aa23c8934bf26791adbc254402ccc02cf3d
tdargent1 <tdargent1@gmail.com>
2020-07-14 19:37:42 +0200

    Ajout formulaire pour la database install

--- app/Controllers/InstallController.php
+++ app/Controllers/InstallController.php
@@ -2,22 +2,32 @@
 
 namespace App\Controllers;
 
-use App\Core\Controller\Controller;
 use App\Core\Http\Request;
 use App\Core\Http\Response;
+use App\Core\Controller\Controller;
 use App\Core\Migration\MigrationRunner;
+use App\Forms\Install\CreateDatabaseForm;
 
 class InstallController extends Controller
 {
     public function showDatabaseForm(Request $request, Response $response)
     {
-        $migrationRunner = new MigrationRunner(DB_PREFIXE);
-        $migrationRunner->run();
-//        return $response->render('admin.install.database', 'account');
+        /*$migrationRunner = new MigrationRunner(DB_PREFIXE);
+        $migrationRunner->run();*/
+
+        $form = $response->createForm(CreateDatabaseForm::class);
+
+        $response->render("admin.install.database", "account", ["createDatabaseForm" => $form]);
     }
 
     public function storeDatabaseForm(Request $request, Response $response)
     {
+        $request->setInputPrefix('createDatabaseForm_');
 
+        $form = $response->createForm(CreateDatabaseForm::class, $user);
+
+        if (false === $form->handle()) {
+            return $response->render("admin.install.database", "account", ["createDatabaseForm" => $form]);
+        }
     }
 }
--- app/Forms/Install/CreateDatabaseForm.php
+++ app/Forms/Install/CreateDatabaseForm.php
@@ -3,8 +3,87 @@
 namespace App\Forms\Install;
 
 use App\Core\Form;
+use App\Core\Routing\Router;
+use App\Core\Constraints\PasswordConstraint;
+use App\Core\Constraints\RequiredConstraint;
 
 class CreateDatabaseForm extends Form
 {
+    public function buildForm()
+    {   
+        $this->setName("createDatabaseForm");
 
+        $this->setBuilder()
+            ->add("adress", "input", [
+                "label" => [
+                    "value" => "Adresse de la base de données"
+                ],
+                "attr" => [
+                    "type" => "text",
+                    "placeholder"=>"Ex : localhost",
+                    "class" => "form-control margin-bottom-25"
+                ],
+                "constraints" => [
+                    new RequiredConstraint()
+                ]
+            ])
+            ->add("name", "input", [
+                "label" => [
+                    "value" => "Nom de la base de données"
+                ],
+                "attr" => [
+                    "type" => "text",
+                    "placeholder"=>"Ex : divineat",
+                    "class" => "form-control margin-bottom-25"
+                ],
+                "constraints" => [
+                    new RequiredConstraint()
+                ]
+            ])
+            ->add("user", "input", [
+                "label" => [
+                    "value" => "Nom d'utilisateur"
+                ],
+                "attr" => [
+                    "type" => "text",
+                    "placeholder"=>"Ex : u-divineat",
+                    "class" => "form-control margin-bottom-25"
+                ],
+                "constraints" => [
+                    new RequiredConstraint()
+                ]
+            ])
+            ->add("password", "input", [
+                "label" => [
+                    "value" => "Nom d'utilisateur"
+                ],
+                "attr" => [
+                    "type" => "password",
+                    "placeholder"=>"Ex : u!-divineat123",
+                    "class" => "form-control margin-bottom-25"
+                ],
+                "constraints" => [
+                    new PasswordConstraint(),
+                    new RequiredConstraint()
+                ]
+            ])
+            ->add("submit", "input", [
+                "attr" => [
+                    "type" => "submit",
+                    "value" => "Enregistrer",
+                    "class" => "btn btn-add right"
+                ]
+            ]);
+    }
+
+    public function configureOptions(): void
+    {
+        $this
+            ->addConfig("attr", [
+                "id" => "createDatabaseForm",
+                "class" => "admin-form width-100",
+                "name" => "createDatabaseForm"
+            ])
+            ->addConfig("action", Router::getRouteByName("install.store-database-form")->getUrl());
+    }
 }
